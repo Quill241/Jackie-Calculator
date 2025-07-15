37277bd7351228d04598260e226c6923a9bcf824
Quill241 <happinessolawunmi@gmail.com>
2025-07-15 08:12:21 +0100

    add main.js file

---
+++ main.js
@@ -0,0 +1,90 @@
+
+
+const savebtn = document.getElementById(save-num)
+
+function add() {
+    const firstNum = document.getElementById("num1")
+    const secondNum = document.getElementById("num2")
+    const output = document.getElementById("output")
+
+    const firstActualNumber = parseInt(firstNum.value)
+    const secondActualNumber = parseInt(secondNum.value)
+
+    let finalAnswer = firstActualNumber + secondActualNumber
+
+    console.log(finalAnswer)
+
+    output.textContent = finalAnswer.toString()
+    return finalAnswer
+
+}
+
+function subtract() {
+    const firstNum = document.getElementById("num1")
+    const secondNum = document.getElementById("num2")
+    const output = document.getElementById("output")
+
+    const firstActualNumber = parseInt(firstNum.value)
+    const secondActualNumber = parseInt(secondNum.value)
+
+    let finalAnswer = firstActualNumber - secondActualNumber
+
+    console.log(finalAnswer)
+
+    output.textContent = finalAnswer.toString()
+    return finalAnswer
+
+}
+
+
+function divide() {
+    const firstNum = document.getElementById("num1")
+    const secondNum = document.getElementById("num2")
+    const output = document.getElementById("output")
+
+    const firstActualNumber = parseInt(firstNum.value)
+    const secondActualNumber = parseInt(secondNum.value)
+
+    let finalAnswer = firstActualNumber / secondActualNumber
+
+    console.log(finalAnswer)
+
+    output.textContent = finalAnswer.toString()
+    return finalAnswer
+
+}
+
+
+function Multiply() {
+    const firstNum = document.getElementById("num1")
+    const secondNum = document.getElementById("num2")
+    const output = document.getElementById("output")
+
+    const firstActualNumber = parseInt(firstNum.value)
+    const secondActualNumber = parseInt(secondNum.value)
+
+    let finalAnswer = firstActualNumber * secondActualNumber
+
+    console.log(finalAnswer)
+
+    output.textContent = finalAnswer.toString()
+    return finalAnswer
+
+}
+
+function modulo() {
+    const firstNum = document.getElementById("num1")
+    const secondNum = document.getElementById("num2")
+    const output = document.getElementById("output")
+
+    const firstActualNumber = parseInt(firstNum.value)
+    const secondActualNumber = parseInt(secondNum.value)
+
+    let finalAnswer = firstActualNumber % secondActualNumber
+
+    console.log(finalAnswer)
+
+    output.textContent = finalAnswer.toString()
+    return finalAnswer
+
+}
